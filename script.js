ecaa451bda4f04d793ba5f52321089892beeb75c
devilhub-123 <novemberking08@gmail.com>
2023-06-09 17:36:13 +0545

    basisc completed

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// script.js
-
-function toggleCourseDetails(htmlCoursesDetails) {
-    var courseDetails = document.getElementById(courses + 'htmlCoursesDetails');
-    if (courseDetails.style.display === 'none') {
-        courseDetails.style.display = 'block';
-    } else {
-        courseDetails.style.display = 'none';
-    }
-
+function scrollToCourses() {
+    var coursesSection = document.getElementById("courses");
+    coursesSection.scrollIntoView({ behavior: 'smooth' });
 }
