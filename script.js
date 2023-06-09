22ff29b1bffb49000d9f83b93233ee3baba09b2d
devilhub-123 <novemberking08@gmail.com>
2023-06-09 21:43:47 +0545

    almost

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-function scrollToCourses() {
-    var coursesSection = document.getElementById("courses");
-    coursesSection.scrollIntoView({ behavior: 'smooth' });
+function showCourseDetails(course) {
+    var courseDetailsSection = document.getElementById("course-details");
+    var courseTitle = document.getElementById("course-title");
+    var courseDescription = document.getElementById("course-description");
+
+    courseTitle.textContent = course.toUpperCase() + " Course";
+    if (course === "html") {
+        courseDescription.textContent = "HTML course description goes here.";
+    } else if (course === "css") {
+        courseDescription.textContent = "CSS course description goes here.";
+    } else if (course === "python") {
+        courseDescription.textContent = "Python course description goes here.";
+    } else if (course === "javascript") {
+        courseDescription.textContent = "JavaScript course description goes here.";
+    } else if (course === "ruby") {
+        courseDescription.textContent = "Ruby course description goes here.";
+    } else {
+        courseDescription.textContent = "";
+    }
+
+    courseDetailsSection.style.display = "block";
+}
+
+function toggleCourseMenu() {
+    var courseMenu = document.getElementById("course-menu");
+    if (courseMenu.style.display === "block") {
+        courseMenu.style.display = "none";
+    } else {
+        courseMenu.style.display = "block";
+    }
 }
