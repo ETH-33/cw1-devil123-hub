7348a2ce38bb6267d81ec7ac3b4aad7a21e7749a
devilhub-123 <novemberking08@gmail.com>
2023-06-10 10:28:38 +0545

    home scevtion almost done

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
 function showCourseDetails(course) {
-    var courseDetailsSection = document.getElementById("course-details");
-    var courseTitle = document.getElementById("course-title");
-    var courseDescription = document.getElementById("course-description");
-
-    courseTitle.textContent = course.toUpperCase() + " Course";
-    if (course === "html") {
-        courseDescription.textContent = "HTML course description goes here.";
-    } else if (course === "css") {
-        courseDescription.textContent = "CSS course description goes here.";
-    } else if (course === "python") {
-        courseDescription.textContent = "Python course description goes here.";
-    } else if (course === "javascript") {
-        courseDescription.textContent = "JavaScript course description goes here.";
-    } else if (course === "ruby") {
-        courseDescription.textContent = "Ruby course description goes here.";
-    } else {
-        courseDescription.textContent = "";
-    }
-
-    courseDetailsSection.style.display = "block";
-}
-
-function toggleCourseMenu() {
-    var courseMenu = document.getElementById("course-menu");
-    if (courseMenu.style.display === "block") {
-        courseMenu.style.display = "none";
-    } else {
-        courseMenu.style.display = "block";
+    var courseContent = document.getElementById(course + "-content");
+    if (courseContent) {
+        var coursesSection = document.getElementById("courses");
+        var htmlContent = document.getElementById("html-content");
+        if (course === "html") {
+            coursesSection.style.display = "none";
+            htmlContent.style.display = "block";
+        } else {
+            coursesSection.style.display = "block";
+            htmlContent.style.display = "none";
+        }
     }
 }
