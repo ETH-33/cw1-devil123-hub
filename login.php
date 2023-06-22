59f5595ca0f40cc70c9a100dc63600939f788f25
Sushant Bhadel <novemberking08@gmail.com>
2023-06-22 10:33:24 +0545

    backend

--- login.php
+++ login.php
@@ -2,3 +2,28 @@
 
 
 ?>
+<html>
+    <head>
+        <title>Login Page </title>
+        <body>
+            <section class ID= Login>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-4">
+                            <div class="login-panel panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Login</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <form class="form-horizontal" role="form" method="post" action="Login.php">
+                                        <fieldset>
+                                            <div class="form-group">
+                                                <label class="col-sm-2 control-label">Username</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" name="username" placeholder="Username">
+                                                </div>
+                                            </div>
+            </section>
+        </body>
+    </head>
+</html>
